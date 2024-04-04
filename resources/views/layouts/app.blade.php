a509e9a62d9bb1cdfec82e9af747898a76d122ec
Raul Astete <raulastete.2215@gmail.com>
2024-04-04 15:26:33 -0500

    Configure tailwindcss and vite

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body class="bg-gray-100">
+    <header class="p-5 border-b bg-white shadow">
+        <div class="container mx-auto flex justify-between">
+            <h1 class="text-3xl font-black">
+                DevStagram
+            </h1>
+
+            <nav class="flex gap-4 items-center">
+                <a class="font-bold uppercase text-gray-600" href="#">Login</a>
+                <a class="font-bold uppercase text-gray-600" href="#">Registration</a>
+            </nav>
+        </div>
+    </header>
+
+    <main class="container mx-auto mt-10">
+        <h2 class="font-black text-center text-3xl mb-10">
+            @yield('title')
+        </h2>
+        @yield('content')
+    </main>
+
+    <footer class="text-center p-5 text-gray-500 font-bold">
+        DevStagram - Todos los derechos reservados {{ date('Y') }}
+    </footer>
+</body>
+
+</html>
