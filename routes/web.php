a51a803a5da1cb96dc714a0a9cb97ef003f4bd83
Raul Astete <raulastete.2215@gmail.com>
2024-04-25 16:19:13 -0500

    Add register routers

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('layouts.app');
 });
 
-Route::get('/register', function () {
-    return view('auth.register');
-})->name('auth.register');
+Route::get('/register', [RegisterController::class, 'index'])->name('register.index');
+Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
