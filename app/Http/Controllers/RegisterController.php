a3ba26d45638dd27814f0cbdc508a5f5110b7651
Raul Astete <raulastete.2215@gmail.com>
2024-04-25 16:31:42 -0500

    Setup register post function

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class RegisterController extends Controller
+{
+    //
+    use ValidatesRequests;
+
+    public function index()
+    {
+        return view('auth.register');
+    }
+
+    public function store(Request $request)
+    {
+
+        $this->validate($request, [
+            'name' => ['required', 'max:30'],
+            'username' => ['required', 'unique:users', 'min:3', 'max:20'],
+            'email' => ['required', 'unique:users', 'email', 'max:60'],
+            'password' => ['required', 'confirmed', 'min:6']
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'username' => Str::slug($request->username),
+            'email' => $request->email,
+            'password' => Hash::make($request->get('password'))
+        ]);
+
+        dd("Usuario creado");
+    }
+}
